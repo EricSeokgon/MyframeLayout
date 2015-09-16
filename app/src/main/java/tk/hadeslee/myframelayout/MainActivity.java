fb20a7edc177d8e82e9a9aefbb801cd880ecc2e3
haseslee <hadesleeq@gmail.com>
2015-09-16 15:40:04 +0900

    Initial commit

---
+++ app/src/main/java/tk/hadeslee/myframelayout/MainActivity.java
@@ -0,0 +1,62 @@
+package tk.hadeslee.myframelayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    ImageView imageView2;
+
+    boolean selected = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        imageView2 = (ImageView) findViewById(R.id.imageView2);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (selected) {
+                    imageView.setVisibility(View.VISIBLE);
+                    imageView2.setVisibility(View.GONE);
+                } else {
+                    imageView.setVisibility(View.GONE);
+                    imageView2.setVisibility(View.VISIBLE);
+                }
+                selected = !selected;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
